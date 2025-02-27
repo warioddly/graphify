a2a452098a8edacab97df4b9a7510a1c29808aa0
warioddly <warioddly@gmail.com>
2025-02-27 19:29:15 +0600

    Implement Echarts Gradient

--- example/lib/charts/basic_area_chart.dart
+++ example/lib/charts/basic_area_chart.dart
@@ -35,41 +35,59 @@ class _BasicAreaChartState extends State<BasicAreaChart> {
   Widget build(BuildContext context) {
     return GraphifyView(
       controller: controller,
-      initialOptions: const {
-        "xAxis": {
+      initialOptions: {
+        "xAxis": const {
           "type": "category",
           "boundaryGap": false,
           "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
         },
-        "yAxis": {
+        "yAxis": const {
           "type": "value"
         },
-        'tooltip': {
+        'tooltip': const {
           'trigger': 'axis',
         },
         "series": [
           {
-            "data": [150, 230, 224, 218, 135, 147, 260],
+            "data": const [150, 230, 224, 218, 135, 147, 260],
             "type": "line",
             'smooth': true,
             'areaStyle': {
-              'color': {
-                'type': 'RadialGradient',
-                'x': 0.1,
-                'y': 0.6,
-                'r': 1,
-                'colorStops': [
-                  {
-                    'color': 'rgba(255, 145, 124, 0.1)',
-                    'offset': 0,
-                  },
-                  {
-                    'color': 'rgba(255, 145, 124, 0.9)',
-                    'offset': 1,
-                  },
+              'color': const GraphifyLinearGradient(
+                x: 0.1,
+                y: 0.6,
+                x2: 0.9,
+                y2: 0.1,
+                colorStops: [
+                  GraphifyGradientColorStop(
+                    color: Colors.green,
+                    offset: 1,
+                  ),
+                   GraphifyGradientColorStop(
+                    color: Colors.red,
+                    offset: .4,
+                  ),
                 ],
-              },
+              ).toJson()
             },
+            // 'areaStyle': {
+            //   'color': {
+            //     'type': 'RadialGradient',
+            //     'x': 0.1,
+            //     'y': 0.6,
+            //     'r': 1,
+            //     'colorStops': [
+            //       {
+            //         'color': 'rgba(255, 145, 124, 0.1)',
+            //         'offset': 0,
+            //       },
+            //       {
+            //         'color': 'rgba(255, 145, 124, 0.9)',
+            //         'offset': 1,
+            //       },
+            //     ],
+            //   },
+            // },
           }
         ]
       },
--- lib/graphify.dart
+++ lib/graphify.dart
@@ -6,3 +6,7 @@ export 'src/controller/implements/facade.dart'
 export 'src/view/implements/facade.dart'
     if (dart.library.io) 'src/view/implements/mobile.dart'
     if (dart.library.html) 'src/view/implements/web.dart';
+
+export 'src/utils/gradient/graphify_gradient.dart';
+export 'src/utils/gradient/graphify_linear_gradient.dart';
+export 'src/utils/gradient/graphify_radial_gradient.dart';
