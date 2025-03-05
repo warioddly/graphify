ddcf7f1a629ece94fda7a641fbf8c90d727751f7
Odinachi David <odinachidavid@yahoo.com>
2025-03-05 16:32:48 +0100

    fix color issue

--- lib/src/utils/color_util.dart
+++ lib/src/utils/color_util.dart
@@ -3,7 +3,7 @@ import 'dart:ui' show Color;
 final class ColorUtils {
 
   static String colorToRgba(Color color) {
-    return 'rgba(${(color.r * 255).toInt()}, ${(color.g * 255).toInt()}, ${(color.b * 255).toInt()}, ${color.a})';
+    return 'rgba(${(color.red * 255).toInt()}, ${(color.green * 255).toInt()}, ${(color.blue * 255).toInt()}, ${color.opacity})';
   }
 
 }
