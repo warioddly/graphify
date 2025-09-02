0e77db567c060d141b8b966c017d0fae72f36070
warioddly <warioddly@gmail.com>
2025-09-02 11:43:01 +0600

    refactor: update conditional imports to use dart.library.js_interop

--- lib/graphify.dart
+++ lib/graphify.dart
@@ -2,9 +2,10 @@ library graphify;
 
 export 'src/controller/implements/facade.dart'
     if (dart.library.io) 'src/controller/implements/mobile.dart'
-    if (dart.library.html) 'src/controller/implements/web.dart';
+    if (dart.library.js_interop) 'src/controller/implements/web.dart';
+
 export 'src/view/implements/facade.dart'
     if (dart.library.io) 'src/view/implements/mobile.dart'
-    if (dart.library.html) 'src/view/implements/web.dart';
+    if (dart.library.js_interop) 'src/view/implements/web.dart';
 
 export 'src/utils/gradient/_gradient.dart';
